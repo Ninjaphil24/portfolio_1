a49a069720a95c90b34da996a97dcce1f1be5fd3
Ninjaphil24 <mphilippos@hotmail.com>
2022-01-25 14:25:01 +0200

    Reviewer's comments done

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const navToggle = () => {
-  const burger = document.querySelector('.toggle');
   const toggler = document.querySelector('.toggle');
-  burger.addEventListener('click', () => {
+  toggler.addEventListener('click', () => {
     toggler.classList.toggle('active');
   });
 };
